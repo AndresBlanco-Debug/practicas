c4167c1ce8c1ece2d26220a74c80ea72506d5452
JBlanco <jblanco@fi.uba.ar>
2023-12-20 18:51:05 -0300

    [ADD] ej3

--- c-c++/calvo/evaluaciones/parciales/ej3_Lista.cpp
+++ c-c++/calvo/evaluaciones/parciales/ej3_Lista.cpp
@@ -4,7 +4,6 @@
 #include "./structs/Pila.h"
 using namespace std;
 /*IMPLEMENTAR LA CLASE A PARTIR DE LAS SIGUIENTES ESPECIFICACCIONES:*/
-
 class Categoria{
 private:
     string nombre;
@@ -14,20 +13,16 @@ public:
     string getNombre();
     string getDescripcion();
 };
-
 Categoria::Categoria(string nombre, string descripcion){
     this->nombre = nombre;
     this->descripcion = descripcion;
 }
-
 string Categoria::getNombre(){
     return this->nombre;
 }
-
 string Categoria::getDescripcion(){
     return this->descripcion;
 }
-
 class Noticia{
 private:
     string titulo;
@@ -41,36 +36,30 @@ public:
     string getCuerpo();
     Lista<Categoria*>* getCategorias();
 };
-
 Noticia::Noticia(string titulo, string cuerpo){
     this->titulo = titulo;
     this->cuerpo = cuerpo;
 }
-
 string Noticia::getTitulo(){
     return this->titulo;
 }
-
 string Noticia::getCuerpo(){
     return this->cuerpo;
 }
-
 Lista<Categoria*>* Noticia::getCategorias(){
     return this->categorias;
 }
-
 class FiltroDeNoticias{
 private:
     Lista<Noticia*>* noticias;
     Lista<Categoria*>* categorias;
 public:
-    //Devuelve las 5 noticias que tengan asociadas TODAS las categorias y que tengan la mayor cantidad de categorias.
+    //CONSIGNA: Devuelve las 5 noticias que tengan asociadas TODAS las categorias y que tengan la mayor cantidad de categorias.
     Lista<Noticia*>* filtrarCategorias(Lista<Noticia*>* noticias, Lista<Categoria*>* categorias);
     Pila<Noticia*>* removerNoticias(Lista<Noticia*>* noticias, Lista<Categoria*>* categorias);
     bool validarCategoria(Noticia* noticia, Lista<Categoria*>* categorias);
     Noticia* mayorCategoria(Lista<Noticia*>* noticias);
 };
-
 bool FiltroDeNoticias::validarCategoria(Noticia* noticia, Lista<Categoria*>* categoriasPedidas){
     bool valido = false;
     unsigned int index = 0;
@@ -90,7 +79,6 @@ bool FiltroDeNoticias::validarCategoria(Noticia* noticia, Lista<Categoria*>* cat
     }
     return valido;
 }
-
 //hay que hacer una funcion que filtre las noticias que tengan todas las categorias.
 Lista<Noticia*>* FiltroDeNoticias::filtrarCategorias(Lista<Noticia*>* noticiasPosibles, Lista<Categoria*>* categoriasElegidas){
     Lista<Noticia*>* notiFiltro = NULL;
@@ -108,10 +96,8 @@ Lista<Noticia*>* FiltroDeNoticias::filtrarCategorias(Lista<Noticia*>* noticiasPo
     }
     return notiFiltro;
 }
-
 //Ahora hay que hacer la funcion para que las noticias seleccionadas se filtren segun categorias;
 //Hacemos una funcion que filtre la mayor y la repetimos 5 veces
-
 Noticia* FiltroDeNoticias::mayorCategoria(Lista<Noticia*>* noticias){
     unsigned int pos = 0;
     Noticia* maxima = noticias->get(1);
@@ -123,3 +109,14 @@ Noticia* FiltroDeNoticias::mayorCategoria(Lista<Noticia*>* noticias){
     }
     noticias->remover(pos);
     return maxima;
+}
+Pila<Noticia*>* FiltroDeNoticias::removerNoticias(Lista<Noticia*>* noticias, Lista<Categoria*>* categorias){
+    Pila<Noticia*>* notiLocas = NULL;
+    Lista<Noticia*>* noticiaFiltradas = filtrarCategorias(noticias, categorias);
+    Noticia* notActual = NULL;
+    for(unsigned int x = 1; x < 6; x++){
+        notActual = mayorCategoria(noticiaFiltradas);
+        notiLocas->agragarElemento(notActual);
+    }
+    return notiLocas;
+}
